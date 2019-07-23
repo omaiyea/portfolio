e4abb8bab01b40344db745b28381948b232b4989
omaiyea <omaiyea@gmail.com>
2019-07-22 23:54:52 -0700

    indicate which page user is on from menu

--- js/index.js
+++ js/index.js
@@ -1,18 +1,29 @@
+//display clicked screenshot at full size
 function renderFullScreenshot(){
-    $('.projects-grid').on('click', 'img', function(){
-        setActiveThumbnail(this);
+    $('#projects-grid').on('click', 'img', function(){
+        setClass(this, 'active-thumbnail');
         $(this).parent().prev().children().attr('src', $(this).attr('src'));
         $(this).parent().prev().children().attr('alt', $(this).attr('alt'));
      });
 }
 
-function setActiveThumbnail(selectedImage){
-    $(selectedImage).addClass('active-thumbnail');
-    $(selectedImage).siblings().removeClass('active-thumbnail');
+//indicate to user which section they are on
+function setSection(){
+    $('nav').on('click', 'li', function(){
+        setClass(this, 'active-page')
+    });
+}
+
+//update styling for elements that can only have one active sibling
+//for example, enlarged thumbnails, current page
+function setClass(selected, className){
+    $(selected).addClass(className);
+    $(selected).siblings().removeClass(className);
 }
 
 function handleApp(){
     renderFullScreenshot();
+    setSection();
 }
 
 $(handleApp);
